9664f546c844cd9ffd83839133a4212966aff83f
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2023-02-22 17:59:18 -0300

    test2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if ((window.innerWidth <= 800) || (window.innerHeight <= 600)) {
+if ((window.innerWidth <= 600) || (window.innerHeight <= 800)) {
 
     window.alert('O site deve ser acesso por um computador')
     document.querySelector('body').innerHTML = 'Acessa pelo PC!!'
